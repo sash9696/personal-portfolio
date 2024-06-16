03e5ec42e0943ff545d09aff50ce2c1b6e784df1
sash9696 <sahilchopra838@gmail.com>
2024-06-16 22:34:43 +0530

    added animations to all the sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
-import about from '../assets/about.jpg';
-import { ABOUT_TEXT } from '../constants';
+import about from "../assets/about.jpg";
+import { ABOUT_TEXT } from "../constants";
+import { motion } from "framer-motion";
 
 function About() {
 	return (
@@ -9,18 +10,29 @@ function About() {
 				<span className="text-neutral-500">Me</span>{" "}
 			</h1>
 
-            <div className="flex flex-wrap">
-                <div className="w-full lg:w-1/2 lg:p-8">
-                    <div className="flex items-center justify-center">
-                        <img className="rounded-2xl" src={about} alt="about" ></img>
-                    </div>
-                </div>
-                <div className='w-full lg:w-1/2'>
-                    <div className='flex justify-center lg:justify-start'>
-                        <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
-                    </div>
-                </div>
-            </div>
+			<div className="flex flex-wrap">
+				<motion.div
+					whileInView={{ opacity: 1, x: 0 }}
+					initial={{ opacity: 0, x: -100 }}
+					transition={{ duration: 0.5 }}
+					className="w-full lg:w-1/2 lg:p-8">
+					<div className="flex items-center justify-center">
+						<img
+							className="rounded-2xl"
+							src={about}
+							alt="about"></img>
+					</div>
+				</motion.div>
+				<motion.div
+					whileInView={{ opacity: 1, x: 0 }}
+					initial={{ opacity: 0, x: 100 }}
+					transition={{ duration: 0.5 }}
+					className="w-full lg:w-1/2">
+					<div className="flex justify-center lg:justify-start">
+						<p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+					</div>
+				</motion.div>
+			</div>
 		</div>
 	);
 }
