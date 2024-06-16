62e75eca96c946fc03b055b0d230a03e77562723
sash9696 <sahilchopra838@gmail.com>
2024-06-16 22:57:51 +0530

    added content in constants

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,16 @@ import {FaLinkedin} from "react-icons/fa";
 import {FaGithub} from "react-icons/fa";
 
 import {FaInstagram} from "react-icons/fa";
+import { NAV_LOGO } from "../constants";
 
 
 function Navbar() {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            {"<SC/>"}
+    <nav className=" mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center text-purple-600 text-4xl">
+            {NAV_LOGO}
         </div>
-        <div className="m-8 flex items-center justify-center text-2xl gap-4">
+        <div className="m-8 flex items-center justify-center text-2xl gap-4 pr-20">
             <FaLinkedin/>
             <FaGithub/>
             <FaInstagram/>
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,74 +3,113 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
+export const NAV_LOGO = "<SC/>";
+
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js,  PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+	{
+		year: "August 2022 - Present",
+		role: "Senior Software Developer",
+		company: "Groundzero Softwares Pvt Ltd",
+		description: `
+        Revamped the product, contributing to a valuation increase to 25 crores.
+        Developed features for a Power BI product, focusing on visualizations, filters, and dashboards.
+        Single-handedly created screens for a healthcare management system.
+        Lead a team of 3 people and 5 interns in product developement, component design.
+        Reduced bundle size by 47% through optimization techniques.
+        Worked on webpack and microfrontend architecture.
+    `,
+		technologies: ["Javascript", "Reactjs", "Node.js"],
+	},
+	{
+		year: "June 2022 - August 2022",
+		role: "Educator",
+		company: "PhysicsWallah",
+		description: `Recorded Javascript Courses. Coordinating with Operations team to curate course content.`,
+		technologies: ["HTML", "CSS", "JS", "Reactjs"],
+	},
+	{
+		year: "March 2022 - June 2022",
+		role: "Software Development Engineer",
+		company: "Oye Rickshaw",
+		description: `
+      Worked with supply team to build new features, integrated APls and solved various bugs in the U.I
+      Created new driver onboarding flow webview using VueJs and integrated it with the android app for drivers.This feature is being used by 7000+ drivers.
+      Single handedly created swap onboarding security pricing feature in admin panel.
+      Optimized the code to decrease the page load time by 12 percent.
+    `,
+		technologies: ["HTML", "CSS", "JS", "Reactjs", "Postgres", "Vuejs"],
+	},
+	{
+		year: "August 2021 - March 2022",
+		role: "Front End Educator",
+		company: "Relevel by Unacademy",
+		description: `
+        Performed live coding and taught Frontend Development to 1000+ students
+        Helped numerous students switch from different domains to frontend development.
+        Coordinating with Subject Matter Experts, Teaching Assistants and Operations team to create courses and content.    
+        `,
+		technologies: ["HTML", "CSS", "JS", "Reactjs"],
+	},
+	{
+		year: "July 2019 - July 2021",
+		role: "Solutions Manager",
+		company: "ICICI Bank",
+		description: `
+        Review and understand requirements related to webpages.
+        Coordinate with third party developers and product managers
+        Testing and reviewing different cms solutions apis
+        Reviewing and creating html webpages, user flows   
+        `,
+		technologies: ["HTML", "CSS", "JS", "Reactjs"],
+	},
+
+	{
+		year: "August 2018 - June 2019",
+		role: "Web Developer",
+		company: "Freelancing",
+		description: `
+    Creating features and resolving Bugs related ot the websites and further optimization of the code. Integrating apis to various webpages. 
+        `,
+		technologies: ["HTML", "CSS", "JS", "Reactjs", "Node"],
+	},
 ];
 
 export const PROJECTS = [
-  {
-    title: "E-Commerce Website",
-    image: project1,
-    description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+	{
+		title: "E-Commerce Website",
+		image: project1,
+		description:
+			"A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+		technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+	},
+	{
+		title: "Task Management App",
+		image: project2,
+		description:
+			"An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+		technologies: ["HTML", "CSS", "Angular", "Firebase"],
+	},
+	{
+		title: "Portfolio Website",
+		image: project3,
+		description:
+			"A personal portfolio website showcasing projects, skills, and contact information.",
+		technologies: ["HTML", "CSS", "React", "Bootstrap"],
+	},
+	{
+		title: "Blogging Platform",
+		image: project4,
+		description:
+			"A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+		technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+	},
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+	phoneNo: "+91-7000911390",
+	email: "sahil.chopra9696@gmail.com",
 };
